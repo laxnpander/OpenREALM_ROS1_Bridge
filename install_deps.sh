b79bd1c99ff07da3f6ec5ed31d1002ad6f1b062d
ykhedar <yogeshkhedar@gmail.com>
2020-05-30 20:11:13 +0200

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -67,6 +67,6 @@ make -j $(nproc --all) && sudo make install
 
 # Finally install OpenREALM
 cd ~ && mkdir OpenREALM && cd OpenREALM
-git clone https://github.com/laxnpander/OpenREALM.git
-cd OpenREALM && mkdir build && cd build && cmake -DTESTS_ENABLED=ON ..
+git clone https://github.com/laxnpander/OpenREALM_ROS1_Bridge.git
+cd OpenREALM_ROS1_Bridge && mkdir build && cd build && cmake -DTESTS_ENABLED=ON ..
 make -j $(nproc --all) sudo make install
