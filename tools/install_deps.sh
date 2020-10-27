b039fa9d3d8b63341a22b2880e50c5f420282854
alex <laxnpander@gmail.com>
2020-10-27 08:51:36 +0100

    Updated Travis CI

--- tools/install_deps.sh
+++ tools/install_deps.sh
@@ -39,6 +39,12 @@ sudo apt install -y -q ros-$ROS_DISTRO-cv-bridge
 sudo apt install -y -q ros-$ROS_DISTRO-rviz
 sudo apt install -y -q ros-$ROS_DISTRO-pcl-ros
 
+# Install specific opencv version for openvslam
+cd ~ && git clone -b 3.3.1 https://github.com/opencv/opencv.git \
+&& cd ~/opencv && mkdir build && cd build \
+&& cmake -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_EXAMPLES=OFF -D BUILD_opencv_apps=OFF -D BUILD_DOCS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D CMAKE_INSTALL_PREFIX=/usr/local .. \
+&& make && sudo make install
+
 # Finally install OpenREALM Librararies
 cd ~ && mkdir OpenREALM && cd OpenREALM
 git clone https://github.com/laxnpander/OpenREALM.git
